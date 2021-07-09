6725d220107f7b081681497ea5fb1d5587675a30
jangbigom91 <jangbigom91@gmail.com>
2021-07-09 17:13:41 +0900

    영화 추천 페이지 step2

--- app.py
+++ app.py
@@ -4,7 +4,38 @@ import json
 from flask import Flask , request , render_template
 from datetime import date
 from sklearn.feature_extraction.text import TfidfVectorizer , CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 from fetch import movie, movie_collection
+# from ml import RECOMMEND
+
+df2 = pd.read_csv('./model/tmdb.csv', encoding='utf-8')
+df2 = df2.reset_index()
+
+count = TfidfVectorizer(stop_words="english")
+count_matrix = count.fit_transform(df2['soup'])
+
+all_titles = [df2['title'][i] for i in range(len(df2['title']))]
+
+indices = pd.Series(df2.index, index=df2['title'])
+
+def get_recommendation(title):
+    cos_sim = cosine_similarity(count_matrix, count_matrix)
+    idx = indices[title]
+
+    sim_scores = list(enumerate(cos_sim[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x : x[1], reverse=True)
+    sim_scores = sim_scores[1:11]
+
+    sim_indics = [i[0] for i in sim_scores]
+    tit = df2['title'].iloc[sim_indics]
+    dat = df2['release_date'].iloc[sim_indics]
+
+    return_df = pd.DataFrame(columns=['Title', 'Year'])
+    return_df['Title'] = tit
+    return_df['Year'] = dat
+
+    return return_df
+
 
 # TMDB 영화데이터
 app = Flask(__name__)
@@ -48,6 +79,7 @@ def index():
         
         return render_template("landing.html", movie=movie_dic.results, key_word=key_word)
 
+
 @app.route('/details/<id>', methods=['GET', 'POST'])
 def details(id):
     url = f"http://api.themoviedb.org/3/movie/{id}?api_key=da396cb4a1c47c5b912fda20fd3a3336"
@@ -56,6 +88,27 @@ def details(id):
     
     # print(data_json)
     return render_template("details.html", movie=data_json)
+
+
+@app.route('/recommend', methods=['GET', 'POST'])
+def recommend():
+    if request.method == 'GET':
+        return render_template('recommend.html')
+    elif request.method == 'POST':
+        m_name = request.form['movie_name']
+        m_name = m_name.title()
+
+        if m_name not in all_titles:
+            return render_template('negative.html', name=m_name)
+        else:
+            result_final = get_recommendation(m_name)
+            data = []
+
+            for i in range(len(result_final)):
+                data.append((result_final.iloc[i][0], result_final.iloc[i][1]))
+            
+            return render_template('positive.html', movie_data=data, search_name=m_name)
+
 
 if __name__ == "__main__":
     app.run(port=5000 ,debug=True)
--- ml.py
+++ ml.py
@@ -3,6 +3,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 df2 = pd.read_csv('./model/tmdb.csv', encoding='utf-8')
+df2 = df2.reset_index()
 
 class RECOMMEND():
     def __init__(self, vectorizer):
@@ -13,8 +14,8 @@ class RECOMMEND():
         count_matrix = count.fit_transform(df2['soup'])
 
         cos_sim = cosine_similarity(count_matrix, count_matrix)
-
         indices = pd.Series(df2.index, index=df2['title'])
+        
         idx = indices[title]
 
         sim_scores = list(enumerate(cos_sim[idx]))
